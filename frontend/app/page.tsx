7c6b44dd0bf9791877ed1d4c4da9bbbc9b8aac24
tugutam55-hue <tugutam55@gmail.com>
2026-02-27 15:18:30 +0700

    Create page.tsx

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import UploadForm from "../components/UploadForm"
+
+export default function Home() {
+  return (
+    <main style={{padding:20}}>
+      <h1>Exam Scanner</h1>
+      <UploadForm />
+    </main>
+  )
+}
